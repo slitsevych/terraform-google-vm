b2bc7d8b1908084c8ce297531e62fb3f0bd86423
Stepan Litsevych <s.litsevych@aimprosoft.com>
2024-08-08 16:26:15 +0300

    Added regional instance template && zonal Mig#1

---
+++ modules/mig_zonal/main.tf
@@ -0,0 +1,240 @@
+locals {
+  healthchecks = concat(
+    google_compute_region_health_check.https[*].self_link,
+    google_compute_region_health_check.http[*].self_link,
+    google_compute_region_health_check.tcp[*].self_link,
+  )
+  autoscaling_scale_in_enabled   = var.autoscaling_scale_in_control.fixed_replicas != null || var.autoscaling_scale_in_control.percent_replicas != null
+  autoscaling_scale_down_enabled = var.autoscaling_scale_down_control.fixed_replicas != null || var.autoscaling_scale_down_control.percent_replicas != null
+}
+
+resource "google_compute_instance_group_manager" "mig" {
+  provider           = google-beta
+  base_instance_name = var.hostname
+  project            = var.project_id
+
+  version {
+    name              = "${var.hostname}-mig-version-0"
+    instance_template = var.instance_template
+  }
+
+  name   = var.mig_name == "" ? "${var.hostname}-mig" : var.mig_name
+  zone   = var.zone
+  dynamic "named_port" {
+    for_each = var.named_ports
+    content {
+      name = lookup(named_port.value, "name", null)
+      port = lookup(named_port.value, "port", null)
+    }
+  }
+  target_pools          = var.target_pools
+  target_size           = var.autoscaling_enabled ? null : var.target_size
+  target_suspended_size = var.autoscaling_enabled ? null : var.target_suspended_size
+  target_stopped_size   = var.autoscaling_enabled ? null : var.target_stopped_size
+
+  wait_for_instances        = var.wait_for_instances
+  wait_for_instances_status = var.wait_for_instances_status 
+
+  dynamic "auto_healing_policies" {
+    for_each = local.healthchecks
+    content {
+      health_check      = auto_healing_policies.value
+      initial_delay_sec = var.health_check["initial_delay_sec"]
+    }
+  }
+
+  dynamic "stateful_disk" {
+    for_each = var.stateful_disks
+    content {
+      device_name = stateful_disk.value.device_name
+      delete_rule = lookup(stateful_disk.value, "delete_rule", null)
+    }
+  }
+
+  dynamic "stateful_internal_ip" {
+    for_each = [for static_ip in var.stateful_ips : static_ip if static_ip["is_external"] == false]
+    content {
+      interface_name = stateful_internal_ip.value.interface_name
+      delete_rule    = lookup(stateful_internal_ip.value, "delete_rule", null)
+    }
+  }
+
+  dynamic "stateful_external_ip" {
+    for_each = [for static_ip in var.stateful_ips : static_ip if static_ip["is_external"] == true]
+    content {
+      interface_name = stateful_external_ip.value.interface_name
+      delete_rule    = lookup(stateful_external_ip.value, "delete_rule", null)
+    }
+  }
+
+  dynamic "update_policy" {
+    for_each = var.update_policy
+    content {
+      max_surge_fixed                = lookup(update_policy.value, "max_surge_fixed", null)
+      max_unavailable_fixed          = lookup(update_policy.value, "max_unavailable_fixed", null)
+      min_ready_sec                  = lookup(update_policy.value, "min_ready_sec", null)
+      replacement_method             = lookup(update_policy.value, "replacement_method", null)
+      most_disruptive_allowed_action = lookup(update_policy.value, "most_disruptive_allowed_action", null)
+      minimal_action                 = update_policy.value.minimal_action
+      type                           = update_policy.value.type
+    }
+  }
+
+  dynamic "standby_policy" {
+    for_each = var.standby_policy
+    content {
+      initial_delay_sec = lookup(standby_policy.value, "initial_delay_sec", 0)
+      mode              = lookup(standby_policy.value, "mode", "MANUAL")
+
+    }
+  }
+
+  all_instances_config {
+    labels = var.labels
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  timeouts {
+    create = var.mig_timeouts.create
+    update = var.mig_timeouts.update
+    delete = var.mig_timeouts.delete
+  }
+}
+
+resource "google_compute_autoscaler" "autoscaler" {
+  provider = google-beta
+  count    = var.autoscaling_enabled ? 1 : 0
+  name     = var.autoscaler_name == "" ? "${var.hostname}-autoscaler" : var.autoscaler_name
+  project  = var.project_id
+  zone     = var.zone
+
+  target = google_compute_instance_group_manager.mig.self_link
+
+  autoscaling_policy {
+    max_replicas    = var.max_replicas
+    min_replicas    = var.min_replicas
+    cooldown_period = var.cooldown_period
+    mode            = var.autoscaling_mode
+    dynamic "scale_in_control" {
+      for_each = local.autoscaling_scale_in_enabled ? [var.autoscaling_scale_in_control] : []
+      content {
+        max_scaled_in_replicas {
+          fixed   = lookup(scale_in_control.value, "fixed_replicas", null)
+          percent = lookup(scale_in_control.value, "percent_replicas", null)
+        }
+        time_window_sec = lookup(scale_in_control.value, "time_window_sec", null)
+      }
+    }
+    dynamic "scale_down_control" {
+      for_each = local.autoscaling_scale_down_enabled ? [var.autoscaling_scale_down_control] : []
+      content {
+        max_scaled_down_replicas {
+          fixed   = lookup(scale_in_control.value, "fixed_replicas", null)
+          percent = lookup(scale_in_control.value, "percent_replicas", null)
+        }
+        time_window_sec = lookup(scale_in_control.value, "time_window_sec", null)
+      }
+    }
+    dynamic "cpu_utilization" {
+      for_each = var.autoscaling_cpu
+      content {
+        target            = lookup(cpu_utilization.value, "target", null)
+        predictive_method = lookup(cpu_utilization.value, "predictive_method", null)
+      }
+    }
+    dynamic "metric" {
+      for_each = var.autoscaling_metric
+      content {
+        name                       = lookup(metric.value, "name", null)
+        target                     = lookup(metric.value, "target", null)
+        type                       = lookup(metric.value, "type", null)
+        single_instance_assignment = lookup(metric.value, "single_instance_assignment", null)
+        filter                     = lookup(metric.value, "filter", null)
+      }
+    }
+    dynamic "load_balancing_utilization" {
+      for_each = var.autoscaling_lb
+      content {
+        target = lookup(load_balancing_utilization.value, "target", null)
+      }
+    }
+    dynamic "scaling_schedules" {
+      for_each = var.scaling_schedules
+      content {
+        disabled              = lookup(scaling_schedules.value, "disabled", null)
+        duration_sec          = lookup(scaling_schedules.value, "duration_sec", null)
+        min_required_replicas = lookup(scaling_schedules.value, "min_required_replicas", null)
+        name                  = lookup(scaling_schedules.value, "name", null)
+        schedule              = lookup(scaling_schedules.value, "schedule", null)
+        time_zone             = lookup(scaling_schedules.value, "time_zone", null)
+      }
+    }
+  }
+}
+
+resource "google_compute_region_health_check" "https" {
+  count   = var.health_check["type"] == "https" ? 1 : 0
+  project = var.project_id
+  name    = var.health_check_name == "" ? "${var.hostname}-https-healthcheck" : var.health_check_name
+
+  check_interval_sec  = var.health_check["check_interval_sec"]
+  healthy_threshold   = var.health_check["healthy_threshold"]
+  timeout_sec         = var.health_check["timeout_sec"]
+  unhealthy_threshold = var.health_check["unhealthy_threshold"]
+
+  https_health_check {
+    port         = var.health_check["port"]
+    request_path = var.health_check["request_path"]
+    host         = var.health_check["host"]
+    response     = var.health_check["response"]
+    proxy_header = var.health_check["proxy_header"]
+  }
+}
+
+resource "google_compute_region_health_check" "http" {
+  count   = var.health_check["type"] == "http" ? 1 : 0
+  project = var.project_id
+  name    = var.health_check_name == "" ? "${var.hostname}-http-healthcheck" : var.health_check_name
+
+  check_interval_sec  = var.health_check["check_interval_sec"]
+  healthy_threshold   = var.health_check["healthy_threshold"]
+  timeout_sec         = var.health_check["timeout_sec"]
+  unhealthy_threshold = var.health_check["unhealthy_threshold"]
+
+  http_health_check {
+    port         = var.health_check["port"]
+    request_path = var.health_check["request_path"]
+    host         = var.health_check["host"]
+    response     = var.health_check["response"]
+    proxy_header = var.health_check["proxy_header"]
+  }
+
+  log_config {
+    enable = var.health_check["enable_logging"]
+  }
+}
+
+resource "google_compute_region_health_check" "tcp" {
+  count   = var.health_check["type"] == "tcp" ? 1 : 0
+  project = var.project_id
+  name    = var.health_check_name == "" ? "${var.hostname}-tcp-healthcheck" : var.health_check_name
+
+  timeout_sec         = var.health_check["timeout_sec"]
+  check_interval_sec  = var.health_check["check_interval_sec"]
+  healthy_threshold   = var.health_check["healthy_threshold"]
+  unhealthy_threshold = var.health_check["unhealthy_threshold"]
+
+  tcp_health_check {
+    port         = var.health_check["port"]
+    request      = var.health_check["request"]
+    response     = var.health_check["response"]
+    proxy_header = var.health_check["proxy_header"]
+  }
+
+  log_config {
+    enable = var.health_check["enable_logging"]
+  }
+}
---
+++ modules/mig_zonal/variables.tf
@@ -0,0 +1,298 @@
+
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+  default     = null
+}
+
+variable "hostname" {
+  description = "Hostname prefix for instances"
+  type        = string
+  default     = "default"
+}
+
+variable "mig_name" {
+  type        = string
+  description = "Managed instance group name. When variable is empty, name will be derived from var.hostname."
+  default     = ""
+}
+
+variable "zone" {
+  description = "The GCP zone where the managed instance group resides."
+  type        = string
+}
+
+variable "instance_template" {
+  description = "Instance template self_link used to create compute instances"
+  type        = string
+}
+
+variable "target_size" {
+  description = "The target number of running instances for this managed instance group. This value should always be explicitly set unless this resource is attached to an autoscaler, in which case it should never be set."
+  type        = number
+  default     = 1
+}
+
+variable "target_suspended_size" {
+  description = "The target number of suspended instances for this managed instance group."
+  type        = number
+  default     = 0
+}
+
+variable "target_stopped_size" {
+  description = "The target number of stopped instances for this managed instance group."
+  type        = number
+  default     = 0
+}
+
+variable "target_pools" {
+  description = "The target load balancing pools to assign this group to."
+  type        = list(string)
+  default     = []
+}
+
+#################
+# Stateful disks
+#################
+variable "stateful_disks" {
+  description = "Disks created on the instances that will be preserved on instance delete. https://cloud.google.com/compute/docs/instance-groups/configuring-stateful-disks-in-migs"
+  type = list(object({
+    device_name = string
+    delete_rule = string
+  }))
+  default = []
+}
+
+#################
+# Stateful IPs
+#################
+variable "stateful_ips" {
+  description = "Statful IPs created on the instances that will be preserved on instance delete. https://cloud.google.com/compute/docs/instance-groups/configuring-stateful-ip-addresses-in-migs"
+  type = list(object({
+    interface_name = string
+    delete_rule    = string
+    is_external    = bool
+  }))
+  default = []
+}
+
+#################
+# Rolling Update
+#################
+
+variable "update_policy" {
+  description = "The rolling update policy. https://www.terraform.io/docs/providers/google/r/compute_region_instance_group_manager#rolling_update_policy"
+  type = list(object({
+    max_surge_fixed                = optional(number)
+    max_unavailable_fixed          = optional(number)
+    min_ready_sec                  = optional(number)
+    replacement_method             = optional(string)
+    minimal_action                 = string
+    type                           = string
+    most_disruptive_allowed_action = optional(string)
+  }))
+  default = []
+}
+
+variable "standby_policy" {
+  description = "The standby policy. https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance_group_manager#standby_policy"
+  type = list(object({
+    initial_delay_sec = optional(number)
+    mode              = optional(string)
+  }))
+  default = []
+}
+
+##############
+# Healthcheck
+##############
+
+variable "health_check_name" {
+  type        = string
+  description = "Health check name. When variable is empty, name will be derived from var.hostname."
+  default     = ""
+}
+
+variable "health_check" {
+  description = "Health check to determine whether instances are responsive and able to do work"
+  type = object({
+    type                = string
+    initial_delay_sec   = number
+    check_interval_sec  = number
+    healthy_threshold   = number
+    timeout_sec         = number
+    unhealthy_threshold = number
+    response            = string
+    proxy_header        = string
+    port                = number
+    request             = string
+    request_path        = string
+    host                = string
+    enable_logging      = bool
+  })
+  default = {
+    type                = ""
+    initial_delay_sec   = 30
+    check_interval_sec  = 30
+    healthy_threshold   = 1
+    timeout_sec         = 10
+    unhealthy_threshold = 5
+    response            = ""
+    proxy_header        = "NONE"
+    port                = 80
+    request             = ""
+    request_path        = "/"
+    host                = ""
+    enable_logging      = false
+  }
+}
+
+#############
+# Autoscaler
+#############
+variable "autoscaler_name" {
+  type        = string
+  description = "Autoscaler name. When variable is empty, name will be derived from var.hostname."
+  default     = ""
+}
+
+variable "autoscaling_enabled" {
+  description = "Creates an autoscaler for the managed instance group"
+  default     = "false"
+  type        = string
+}
+
+variable "max_replicas" {
+  description = "The maximum number of instances that the autoscaler can scale up to. This is required when creating or updating an autoscaler. The maximum number of replicas should not be lower than minimal number of replicas."
+  default     = 10
+  type        = number
+}
+
+variable "min_replicas" {
+  description = "The minimum number of replicas that the autoscaler can scale down to. This cannot be less than 0."
+  default     = 2
+  type        = number
+}
+
+variable "cooldown_period" {
+  description = "The number of seconds that the autoscaler should wait before it starts collecting information from a new instance."
+  default     = 60
+  type        = number
+}
+
+variable "autoscaling_mode" {
+  description = "Operating mode of the autoscaling policy. If omitted, the default value is ON. https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler#mode"
+  type        = string
+  default     = null
+}
+
+variable "autoscaling_cpu" {
+  description = "Autoscaling, cpu utilization policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler#cpu_utilization"
+  type = list(object({
+    target            = number
+    predictive_method = string
+  }))
+  default = []
+}
+
+variable "autoscaling_metric" {
+  description = "Autoscaling, metric policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler#metric"
+  type = list(object({
+    name   = string
+    target = number
+    type   = string
+    single_instance_assignment = number
+    filter = string
+  }))
+  default = []
+}
+
+variable "autoscaling_lb" {
+  description = "Autoscaling, load balancing utilization policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler#load_balancing_utilization"
+  type        = list(map(number))
+  default     = []
+}
+
+variable "scaling_schedules" {
+  description = "Autoscaling, scaling schedule block. https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler#scaling_schedules"
+  type = list(object({
+    disabled              = bool
+    duration_sec          = number
+    min_required_replicas = number
+    name                  = string
+    schedule              = string
+    time_zone             = string
+  }))
+  default = []
+}
+
+variable "autoscaling_scale_in_control" {
+  description = "Autoscaling, scale-in control block. https://www.terraform.io/docs/providers/google/r/compute_autoscaler#scale_in_control"
+  type = object({
+    fixed_replicas   = number
+    percent_replicas = number
+    time_window_sec  = number
+  })
+  default = {
+    fixed_replicas   = null
+    percent_replicas = null
+    time_window_sec  = null
+  }
+}
+
+variable "autoscaling_scale_down_control" {
+  description = "Autoscaling, scale-down control block. https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler#nested_scale_down_control"
+  type = object({
+    fixed_replicas   = number
+    percent_replicas = number
+    time_window_sec  = number
+  })
+  default = {
+    fixed_replicas   = null
+    percent_replicas = null
+    time_window_sec  = null
+  }
+}
+
+##########################
+
+variable "named_ports" {
+  description = "Named name and named port. https://cloud.google.com/load-balancing/docs/backend-service#named_ports"
+  type = list(object({
+    name = string
+    port = number
+  }))
+  default = []
+}
+
+variable "wait_for_instances" {
+  description = "Whether to wait for all instances to be created/updated before returning. Note that if this is set to true and the operation does not succeed, Terraform will continue trying until it times out."
+  type        = string
+  default     = "false"
+}
+
+variable "wait_for_instances_status" {
+  description = "When used with wait_for_instances it specifies the status to wait for. When STABLE is specified this resource will wait until the instances are stable before returning. When UPDATED is set, it will wait for the version target to be reached and any per instance configs to be effective as well as all instances to be stable before returning. The possible values are STABLE and UPDATED"
+  type        = string
+  default     = null
+}
+
+variable "mig_timeouts" {
+  description = "Times for creation, deleting and updating the MIG resources. Can be helpful when using wait_for_instances to allow a longer VM startup time. "
+  type = object({
+    create = string
+    update = string
+    delete = string
+  })
+  default = {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "Labels, provided as a map"
+  default     = {}
+}
